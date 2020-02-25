292fc9985515aafa3f1e7bf7a558bd07843d56d1
Andrey Khanin <andrey.khanin@dataart.com>
2020-02-25 10:27:21 +0300

    Polish

--- StartupBudget/StartupBudget.Domain/Models/Developer.cs
+++ StartupBudget/StartupBudget.Domain/Models/Developer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,11 @@ namespace StartupBudget.Domain.Models
     public class Developer
     {
         public int Id { get; set; }
-
+        [Required]
         public string FullName { get; set; }
 
         public int WeekRate { get; set; }
-
+        [Required]
         public string Qualification { get; set; }
     }
 }
