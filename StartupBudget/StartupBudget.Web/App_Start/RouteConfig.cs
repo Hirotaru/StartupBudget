21ca13ce97ef35400054f55349752043cb3e57b8
Andrey Khanin <andrey.khanin@dataart.com>
2020-02-26 14:55:36 +0300

    New start page

--- StartupBudget/StartupBudget.Web/App_Start/RouteConfig.cs
+++ StartupBudget/StartupBudget.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace StartupBudget.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Developers", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
