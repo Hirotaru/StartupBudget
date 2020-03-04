7b295eee002c6e6481d836597bc04cc2a81f6565
Andrey Khanin <andrey.khanin@dataart.com>
2020-03-04 11:26:33 +0300

    Database updated

---
+++ StartupBudget/StartupBudget.DAL/Migrations/202003040824474_ChangedNameRequirements.cs
@@ -0,0 +1,20 @@
+﻿namespace StartupBudget.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ChangedNameRequirements : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Developers", "FirstName", c => c.String(nullable: false, maxLength: 255));
+            AlterColumn("dbo.Developers", "LastName", c => c.String(nullable: false, maxLength: 255));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Developers", "LastName", c => c.String(nullable: false));
+            AlterColumn("dbo.Developers", "FirstName", c => c.String(nullable: false));
+        }
+    }
+}
