5c7641d96571c858416c1f767e5c2f8054d6fab1
Andrey Khanin <andrey.khanin@dataart.com>
2020-03-05 16:43:01 +0300

    Added Project class

---
+++ StartupBudget/StartupBudget.Domain/Project/Project.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartupBudget.Domain.Project
+{
+    public class Project
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime From { get; set; }
+
+        public DateTime To { get; set; }
+    }
+}
